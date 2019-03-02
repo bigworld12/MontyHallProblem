78cebd34eff961113a0560eff38e698dab2f6ab2
Ahmed Fwela <ahmednfwela@gmail.com>
2019-03-02 07:03:49 +0200

    Final Edits

--- src/Program.cs
+++ src/Program.cs
@@ -9,11 +9,12 @@ namespace MontyHallProblem
         {
             do
             {
+                Console.WriteLine($"Testing {Tries:N0} Tries");
                 TestAlwaysStay();
                 TestAlwaysSwitch();
                 TestRandomStaySwitch();
-                Console.WriteLine("Press S to repeat the expirement");
-                if (Console.ReadKey(true).KeyChar != 's')
+                Console.WriteLine("Press R to repeat the expirement");
+                if (Console.ReadKey(true).KeyChar != 'r')
                 {
                     return;
                 }
@@ -27,26 +28,21 @@ namespace MontyHallProblem
             Console.WriteLine("=========================================");
             Console.WriteLine("============ Test Awalys Stay ===========");
             Console.WriteLine("=========================================");
-            
-            Dictionary<int, Count> SuccessTimes = new Dictionary<int, Count>();
-            foreach (var item in DoorsCount)
+
+            Dictionary<int, int> SuccessTimes = new Dictionary<int, int>();
+
+            foreach (var doors in DoorsCount)
             {
-                SuccessTimes[item] = new Count();
-            }
-             
-            foreach (var entry in SuccessTimes)
-            {
-                var successCount = entry.Value;
                 var temp = 0;
                 for (int i = 0; i < Tries; i++)
                 {
-                    var correctDoor = RNG(1, entry.Key);
-                    var pickedDoor = RNG(1, entry.Key);
+                    var correctDoor = RNG_Inclusive(1, doors);
+                    var pickedDoor = RNG_Inclusive(1, doors);
                     if (correctDoor == pickedDoor)
                         temp++;
                 }
-                successCount.Num = temp;
-                Console.WriteLine($"Success for {entry.Key} doors: {successCount.Num} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
+                SuccessTimes[doors] = temp;
+                Console.WriteLine($"Success for {doors:N0} doors: {temp:N0} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
             }
             Console.WriteLine("=========================================");
         }
@@ -56,25 +52,20 @@ namespace MontyHallProblem
             Console.WriteLine("=========== Test Awalys Switch ==========");
             Console.WriteLine("=========================================");
             //repeat 10000 times
-            Dictionary<int, Count> SuccessTimes = new Dictionary<int, Count>();
-            foreach (var item in DoorsCount)
+            Dictionary<int, int> SuccessTimes = new Dictionary<int, int>();
+
+            foreach (var doors in DoorsCount)
             {
-                SuccessTimes[item] = new Count();
-            }
-            foreach (var entry in SuccessTimes)
-            {
-             
-                var successCount = entry.Value;
                 var temp = 0;
                 for (int i = 0; i < Tries; i++)
                 {
-                    var correctDoor = RNG(1, entry.Key);
-                    var pickedDoor = RNG(1, entry.Key);
+                    var correctDoor = RNG_Inclusive(1, doors);
+                    var pickedDoor = RNG_Inclusive(1, doors);
                     if (correctDoor != pickedDoor) //when switching, you are checking if the other door is the correct one, and the picked door is the wrong one
                         temp++;
                 }
-                successCount.Num = temp;
-                Console.WriteLine($"Success for {entry.Key} doors: {successCount.Num} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
+                SuccessTimes[doors] = temp;
+                Console.WriteLine($"Success for {doors:N0} doors: {temp:N0} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
             }
             Console.WriteLine("=========================================");
         }
@@ -84,37 +75,29 @@ namespace MontyHallProblem
             Console.WriteLine("=============== Test Random =============");
             Console.WriteLine("=========================================");
             //repeat 10000 times
-            Dictionary<int, Count> SuccessTimes = new Dictionary<int, Count>();
-            foreach (var item in DoorsCount)
+            Dictionary<int, int> SuccessTimes = new Dictionary<int, int>();
+            foreach (var doors in DoorsCount)
             {
-                SuccessTimes[item] = new Count();
-            }
-            foreach (var entry in SuccessTimes)
-            {
-                var successCount = entry.Value;
                 var temp = 0;
                 for (int i = 0; i < Tries; i++)
                 {
-                    var correctDoor = RNG(1, entry.Key);
-                    var pickedDoor = RNG(1, entry.Key);
-                    var switchOrStay = RNG(0, 1); //0  is stay, 1 is switch
+                    var correctDoor = RNG_Inclusive(1, doors);
+                    var pickedDoor = RNG_Inclusive(1, doors);
+                    var switchOrStay = RNG_Inclusive(0, 1); //0  is stay, 1 is switch
                     if ((switchOrStay == 0 && correctDoor == pickedDoor) || (switchOrStay == 1 && correctDoor != pickedDoor))
                         temp++;
                 }
-                successCount.Num = temp;
-                Console.WriteLine($"Success for {entry.Key} doors: {successCount.Num} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
+                SuccessTimes[doors] = temp;
+                Console.WriteLine($"Success for {doors:N0} doors: {temp:N0} ({Math.Round(100d * temp / Tries, 2)}% success rate)");
             }
             Console.WriteLine("=========================================");
         }
         static readonly Random r = new Random();
-        public static int RNG(int min, int max)
+        public static int RNG_Inclusive(int min, int max)
         {
-            return r.Next(min, max);
+            return r.Next(min, max + 1);
         }
-        public class Count
-        {
-            public int Num;
-        }
+
     }
 
 }
---
+++ src/Results.md
@@ -0,0 +1,117 @@
+﻿# The Monty Hall Problem
+- Read about it from here : https://en.wikipedia.org/wiki/Monty_Hall_problem
+- Or watch numberphile videos :
+  - for dummies : https://www.youtube.com/watch?v=7u6kFlWZOWg
+  - for not-so-dummies : https://www.youtube.com/watch?v=4Lb-6rxZxx0
+  - for hardcore-not-so-dummies : https://www.youtube.com/watch?v=ugbWqWCcxrg
+
+# What this repo does:
+this basically just creates random scenarios (10,000 tries), it chooses a random correct door, and a random picked door at first, then based on the approach you take (either switch or stay or random switch/stay), it checks if you picked the correct door or not.
+
+# Explaination:
+in general, this is basically a question of, what's the probability of choosing the same number twice from a set of numbers
+
+if that probability is LOW, then you should go with SWITCHING
+
+if that probability is HIGH, then you should go with STAYING
+```
+lets say that we have 3 numbers a,b and c
+these are all the possible combinations
+N1          | a | a | a | b | b | b | c | c | c
+N2          | a | b | c | a | b | c | a | b | c
+Is Equal ?  | T | F | F | F | T | F | F | F | T
+
+we have 3/9=1/3 chance to get it right, so we should go with switching
+```
+that's the theory people have debated over the years, so i created this repo to show its actual effectiveness in real scenarios 
+
+
+# Results:
+#### here are some results if you are too lazy to run the code yourself
+ - Run 1
+```
+Testing 10,000 Tries
+=========================================
+============ Test Awalys Stay ===========
+=========================================
+Success for 3 doors: 3,346 (33.46% success rate)
+Success for 10 doors: 1,012 (10.12% success rate)
+Success for 100 doors: 83 (0.83% success rate)
+Success for 10,000 doors: 0 (0% success rate)
+=========================================
+=========================================
+=========== Test Awalys Switch ==========
+=========================================
+Success for 3 doors: 6,683 (66.83% success rate)
+Success for 10 doors: 9,009 (90.09% success rate)
+Success for 100 doors: 9,912 (99.12% success rate)
+Success for 10,000 doors: 9,999 (99.99% success rate)
+=========================================
+=========================================
+=============== Test Random =============
+=========================================
+Success for 3 doors: 4,958 (49.58% success rate)
+Success for 10 doors: 5,066 (50.66% success rate)
+Success for 100 doors: 5,079 (50.79% success rate)
+Success for 10,000 doors: 5,107 (51.07% success rate)
+=========================================
+```
+ - Run 2
+```
+Testing 10,000 Tries
+=========================================
+============ Test Awalys Stay ===========
+=========================================
+Success for 3 doors: 3,322 (33.22% success rate)
+Success for 10 doors: 1,022 (10.22% success rate)
+Success for 100 doors: 79 (0.79% success rate)
+Success for 10,000 doors: 1 (0.01% success rate)
+=========================================
+=========================================
+=========== Test Awalys Switch ==========
+=========================================
+Success for 3 doors: 6,750 (67.5% success rate)
+Success for 10 doors: 8,983 (89.83% success rate)
+Success for 100 doors: 9,893 (98.93% success rate)
+Success for 10,000 doors: 9,999 (99.99% success rate)
+=========================================
+=========================================
+=============== Test Random =============
+=========================================
+Success for 3 doors: 4,968 (49.68% success rate)
+Success for 10 doors: 5,060 (50.6% success rate)
+Success for 100 doors: 4,961 (49.61% success rate)
+Success for 10,000 doors: 5,040 (50.4% success rate)
+=========================================
+```
+ - Run 3
+```
+Testing 10,000 Tries
+=========================================
+============ Test Awalys Stay ===========
+=========================================
+Success for 3 doors: 3,308 (33.08% success rate)
+Success for 10 doors: 956 (9.56% success rate)
+Success for 100 doors: 110 (1.1% success rate)
+Success for 10,000 doors: 0 (0% success rate)
+=========================================
+=========================================
+=========== Test Awalys Switch ==========
+=========================================
+Success for 3 doors: 6,690 (66.9% success rate)
+Success for 10 doors: 8,982 (89.82% success rate)
+Success for 100 doors: 9,875 (98.75% success rate)
+Success for 10,000 doors: 9,997 (99.97% success rate)
+=========================================
+=========================================
+=============== Test Random =============
+=========================================
+Success for 3 doors: 5,017 (50.17% success rate)
+Success for 10 doors: 5,080 (50.8% success rate)
+Success for 100 doors: 5,047 (50.47% success rate)
+Success for 10,000 doors: 5,000 (50% success rate)
+=========================================
+```
+# Conclusion:
+it becomes more apparent that the more numbers you have, the less probable you get the correct number,
+# ***So in general, switching is the most logical approach to take***
